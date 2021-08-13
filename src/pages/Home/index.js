c30de4be44c70a1199052cc951b06ee79b120564
Kaio <kaio.siqueira@poli,ufrj.br>
2021-08-13 13:53:47 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import * as S from './styled';
+import { useHistory } from 'react-router-dom';
+
+
+function App(props) {
+  const history = useHistory();
+  const [usuario, setUsuario] = useState('');
+  const [ erro, setErro] = useState(false);
+
+  function handlePesquisa() {
+    axios.get(`https://api.github.com/users/${usuario}/repos`)
+      .then(response => {
+      const repositories = response.data;
+      const repositoriesName = [];
+      repositories.forEach((repository) => {
+        repositoriesName.push(repository.name);
+      });
+      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+      setErro(false);
+      history.push('./repositories');
+    })
+    .catch(err => {
+      setErro(true);
+    });
+  }
+
+  return (
+    <S.HomeContainer>
+        <S.Content>
+          <S.h1>Pesquisa de Repositórios no GitHub</S.h1>
+        </S.Content>
+        <S.Content>
+        <S.Input name="usuario" id="usuario" className="usuarioInput " placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)} />
+        <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+      </S.Content>
+      { erro ? <S.ErrorMsg>Ocorreu um Erro. Tente novamente</S.ErrorMsg> : ''}
+    </S.HomeContainer>
+
+  );
+}
+
+export default App;
---
+++ src/pages/Home/styled.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+    height: 100vh;
+    flex-direction: column;
+    display: flex;
+    align-items: center;
+    justify-content: center; 
+`;
+
+export const h1 = styled.h1`
+    font-family: Sans-serif;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+export const Content = styled.div`
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Input = styled.input`
+    border: 1px solid #ddd;
+    height: 1.5rem;
+    padding: 0 .5rem;
+    border-radius: .25rem 0 0 .25rem;
+    font-family: Sans-serif;
+
+    &:focus,
+    &:active {
+        outline: none;
+        box-shadow: none;
+    }
+`;
+
+export const Button = styled.button`
+    height: 1.5rem;
+    border: 1px solid #000;
+    background: #000;
+    color: #fff;
+    border-radius: 0 .25rem .25rem 0;
+    font-family: Sans-serif;
+    
+
+    &:focus,
+    &:active {
+        outline: none;
+        box-shadow: none;
+    }
+`;
+
+export const ErrorMsg = styled.span`
+    display: block;
+    font-size: 1rem;
+    color: red;
+    font-weight: 600;
+    margin-top: 1rem;
+    font-family: Sans-serif;
+`;
